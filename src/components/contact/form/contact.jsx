cd3b87b5a64f97e8fdd11a0c29a58cf86c67d3f4
Jhon Byron <jhoncolombia66@gmail.com>
2023-10-20 21:16:32 -0300

    styled contact form

--- src/components/contact/form/contact.jsx
+++ src/components/contact/form/contact.jsx
@@ -1,17 +1,38 @@
-import Button from "../../layout/button/button"
+import Button from "../../layout/button/button";
 
 export default function Contact() {
-  return(
-    <div className="flex flex-col items-center bg-gray-50 p-36 w-11/12 h-96 ml-[800px] mb-20">
-      <form className="" action="">
-        <h4 className="text-2xl">Nome</h4>
-        <input type="name" placeholder="Digite teu nome" />
-        <h4>Email</h4>
-        <input type="email" placeholder="Digite teu email" />
-        <h4>Mensagem</h4>
-        <input type="text" placeholder="Digite sua mensagem" />
+  const handleSubmit = (data) => {
+    console.log(data, "clicou aqui");
+  };
+
+  return (
+    <div className=" bg-gray-100 flex flex-col items-center">
+      <form className="p-20 m-4 w-2/5 flex flex-col rounded-xl bg-gray-50 text-black">
+        <h4 className="text-xl p-1 font-One ">Nome</h4>
+        <input
+          className="text-gray-950 m-1 p-2 rounded-xl font-sans border-black "
+          type="name"
+          placeholder="Digite teu nome"
+        />
+        <h4 className="text-xl p-1  font-One">Email</h4>
+        <input
+          className="text-gray-950 m-1 p-2 rounded-xl font-sans border-black"
+          type="email"
+          placeholder="Digite teu email"
+        />
+        <h4 className="text-xl p-1 font-One ">Mensagem</h4>
+        <div>
+          <textarea
+            className="resize-none w-full p-5 m-1  box-border border-2 rounded-xl border-transparent font-sans bg-white"
+            placeholder="Digite sua mensagem"
+            rows="10"
+          />
+          <div className="flex flex-row-reverse">
+          <Button>Enviar</Button>
+          </div>
+         
+        </div>
       </form>
-      <Button>Enviar</Button>
     </div>
-  )
+  );
 }
--- src/components/layout/button/button.jsx
+++ src/components/layout/button/button.jsx
@@ -1,6 +1,10 @@
 export default function Button({ children, ...props }) {
-  return(
-    <button {...props} className="bg-white hover:bg-black hover:text-white hover:border-white hover:scale-110 ease-in-out duration-700 text-black font-bold p-4 m-20 w-56  rounded-full border-[2px] border-black"
-    >{children}</button>
-  )
+  return (
+    <button
+      {...props}
+      className=" bg-white hover:bg-black hover:text-white hover:border-white hover:scale-110 ease-in-out duration-500 text-black font-bold p-4 mt-8 w-56  rounded-full border-[2px] border-black"
+    >
+      {children}
+    </button>
+  );
 }
